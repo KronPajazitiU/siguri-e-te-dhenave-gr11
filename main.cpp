98f5bef245a2551fc683fd4b008438d327627a6b
Arta Gashi <arta.gashi27@student.uni-pr.edu>
2026-04-18 19:47:58 +0200

    Update main.cpp
    Kjo pjese e kodit e kryen procesimin e tekstit

--- main.cpp
+++ main.cpp
@@ -3,4 +3,20 @@
 #include <string>
 #include <vector>
 
+//text processing
+string processBeaufort(string text, string key) {
+    string result = "";
+    int keyIndex = 0;
+
+    for (int i = 0; i < text.length(); i++) {
+        if (isalpha(text[i])) {
+            result += transformChar(text[i], key[keyIndex % key.length()]);
+            keyIndex++;
+        } else {
+            result += text[i];
+        }
+    }
+    return result;
+}
+
 using namespace std;
