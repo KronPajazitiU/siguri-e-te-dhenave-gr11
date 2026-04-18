6729f33c0eb04be98598185fdfefc824b2a29cde
kaltrinabunjaku <kaltrina.bunjaku6@student.uni-pr.edu>
2026-04-18 22:50:07 +0200

    logjika matematike

--- main.cpp
+++ main.cpp
@@ -4,3 +4,18 @@
 #include <vector>
 
 using namespace std;
+
+// -- LOGJIKA MATEMATIKE ---
+char transformChar(char p, char k) {
+    if (!isalpha(p)) return p;
+
+    bool isLower = islower(p);
+    char P = toupper(p);
+    char K = toupper(k);
+
+    // Formula: C = (K - P + 26) % 26
+    int cIndex = (K - P + 26) % 26;
+    char res = (char)(cIndex + 'A');
+
+    return isLower ? (char)tolower(res) : res;
+}
